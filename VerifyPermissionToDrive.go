34b1bbea02f457f841e84181549e2643bedfe096
Cristian Silva <cristian123105@gmail.com>
2020-06-27 12:25:57 -0300

    add contact to array

--- VerifyPermissionToDrive.go
+++ VerifyPermissionToDrive.go
@@ -1,9 +1,19 @@
 package main //nome do pacote
 
-import "fmt" //formatação
+import (
+	"fmt"
+	"strconv"
+) //formatação
+
+type Contact struct {
+	name  string
+	phone int
+	age   int
+}
 
 func main() {
 
+	contacts := [50]Contact{}
 	var options int = 1
 
 	for options != 0 {
@@ -14,9 +24,50 @@ func main() {
 		fmt.Println("2 - Mostrar todos os contatos")
 		fmt.Println("3 - Remover um contato")
 		fmt.Println("0 - Sair")
+		fmt.Print("-> ")
 		fmt.Scanln(&options)
-		fmt.Println(options)
+
+		switch options {
+		case 1:
+			var name string
+			var phone int
+			var age int
+			var position int
+
+			fmt.Println("Qual a posição desse contato ?")
+			fmt.Scanln(&position)
+
+			fmt.Println("Nome:")
+			fmt.Scanln(&name)
+
+			fmt.Println("Telefone:")
+			fmt.Scanln(&phone)
+
+			fmt.Println("Idade: ")
+			fmt.Scanln(&age)
+
+			contacts[position].name = name
+			contacts[position].phone = phone
+			contacts[position].age = age
+
+			fmt.Println("Contao adicionado:\n")
+			fmt.Println("Nome: " + name + "\n Idade: " + strconv.Itoa(age) + "\n Telefone: " + strconv.Itoa(phone))
+
+		case 2:
+			fmt.Println("Mostrar todos os contatos")
+		case 3:
+			fmt.Println("Remover um contato")
+		default:
+			fmt.Println("Insira uma opção válida\n")
+		}
 
 	}
-
 }
+
+/*
+pegar dados do menu - ok
+switch para as operacações
+adicionar no array
+tirar do array
+mostrar o conteudo do array
+*/
